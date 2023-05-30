baf3dc58b351db1cb81fb39c7f07cab62aac5d3a
deepak <deepakrajesh567@gmail.com>
2023-05-30 18:54:42 +0530

    Dynamically added flas news description to the flash news section

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 let breakingImage = document.querySelector("#breakingImg");
+
 let breakingNewsTitle = document.querySelector("#breakingNews .title");
+
+let breakingNewsDesc = document.querySelector("#breakingNews .description");
 
 
 /** API KEY*/
@@ -7,7 +10,7 @@ const apiKey = "191f38e21db040aea2148a0a1e4e09a1";
 
 /*Async function to fetch data*/
 const fetchData = async (categry,pageSize)=>{
-    const url = `https://newsapi.org/v2/top-headlines?country=us&category=${categry}&pageSize=${pageSize}&apiKey=${apiKey}`;
+    const url = `https://newsapi.org/v2/top-headlines?country=in&category=${categry}&pageSize=${pageSize}&apiKey=${apiKey}`;
     const data = await fetch(url);
     let response = await data.json();
     console.log(response);
@@ -15,10 +18,11 @@ const fetchData = async (categry,pageSize)=>{
 
 }
 
-/**Adding Flash newsImage to the Flash News Section */
+/**Adding Flash news to the Flash News Section */
 const insertFlashNews = (data)=>{
-    breakingImage.innerHTML = `<img src="${data[0].urlToImage}" alt="">`;
+    breakingImage.innerHTML = `<img src="${data[0].urlToImage}" alt="">`;//Addidng Flash news Image to the flash news section
     breakingNewsTitle.innerHTML = `<h2>${data[0].content}</h2>`; // Adding Flash News Title to the page
+    breakingNewsDesc.innerHTML =`${data[0].description}`;
 }
-fetchData('general',5).then(insertFlashNewsImage); 
+fetchData('general',5).then(insertFlashNews); 
 
