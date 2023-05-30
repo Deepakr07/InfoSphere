be93756cf2197c1e8b0f041b12e184deb96b68f6
deepak <deepakrajesh567@gmail.com>
2023-05-30 17:58:51 +0530

    Fetched data from the news api

---
+++ script.js
@@ -0,0 +1,17 @@
+
+
+
+
+
+/** API KEY*/
+const apiKey = "191f38e21db040aea2148a0a1e4e09a1";
+
+/*Async function to fetch data*/
+const fetchData = async (categry,pageSize)=>{
+    const url = `https://newsapi.org/v2/top-headlines?country=us&category=${categry}&pageSize=${pageSize}&apiKey=${apiKey}`;
+    const data = await fetch(url);
+    let response = await data.json();
+    console.log(response);
+
+}
+// fetchData('general',5); 
